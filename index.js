ab5cf8f24c7ec76c5318b1648d13138cc60e230f
Isaac <isaackomeza@gmail.com>
2020-09-21 08:28:59 +0200

    ignoring node_modules

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static("client/build"));
+
 if (process.env.NODE_ENV === "production") {
   //serve static content
   //npm run build
@@ -93,5 +94,8 @@ app.delete("/todos/:id", async (req, res) => {
     }
   });
 
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build/index.html"));
+  });
 
 app.listen(PORT, console.log(`server has started on port ${PORT}`));
