abfca8641967b4e2d2839dac504319ce9e80105d
Isaac <isaackomeza@gmail.com>
2020-09-21 09:26:00 +0200

    adding input todo

---
+++ client/src/components/InputTodo.js
@@ -0,0 +1,45 @@
+import React, { Fragment, useState } from "react";
+
+const InputTodo = () => {
+  const [description, setDescription] = useState("");
+
+  const onSubmitForm = async e => {
+    e.preventDefault();
+    try {
+      const body = { description };
+
+      //proxy is only use in development so it will be ignored in production
+      //so if there is no http://localhost:5000 then by default it is going to use heroku domain
+      //remember this heroku app is just our server serving the build static content and also holding the restful api
+
+      //https://pern-todo-app-demo.herokuapp.com/todos
+
+      const response = await fetch("/todos", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body)
+      });
+
+      window.location = "/";
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  return (
+    <Fragment>
+      <h1 className="text-center mt-5">Isaac's PERN TODO List</h1>
+      <form className="d-flex mt-5" onSubmit={onSubmitForm}>
+        <input
+          type="text"
+          className="form-control mr-3"
+          value={description}
+          onChange={e => setDescription(e.target.value)}
+        />
+        <button className="btn btn-success">Add</button>
+      </form>
+    </Fragment>
+  );
+};
+
+export default InputTodo;
