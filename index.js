2894ffd743581ce8624daec7f642f44319b03ad6
Isaac <isaackomeza@gmail.com>
2020-09-21 11:14:45 +0200

    fixing node module issue

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static("client/build"));
+
 
 if (process.env.NODE_ENV === "production") {
   //serve static content
